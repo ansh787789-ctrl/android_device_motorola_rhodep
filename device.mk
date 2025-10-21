1dc9dc2afa0290d10faddef9a1a2cdb880cc698e
ansh787789-ctrl <ansh787789@gmail.com>
2025-10-21 11:59:57 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -122,3 +122,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/tcpc_sgm7220.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/tcpc_sgm7220.ko \
     $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/ldo_vibrator_mmi.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/ldo_vibrator_mmi.ko \
     $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/utags.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/utags.ko
+    $(LOCAL_PATH)/recovery/root/vendor/lib/modules/1.1/nvt_36xxx.ko:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/lib/modules/1.1/nvt_36xxx.ko
